f0aeea980393a36468f16a1b731ab286bd20b2a8
FazzyPlayzMc <fazzyplayzmc@gmail.com>
2021-06-25 17:07:56 +0400

    Moved back to GitHub

---
+++ src/main/java/me/fazzyplayzmc/core/commands/GodCmd.java
@@ -0,0 +1,61 @@
+package me.fazzyplayzmc.core.commands;
+
+import me.fazzyplayzmc.core.Core;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class GodCmd implements CommandExecutor {
+    private Core plugin;
+
+    public GodCmd(Core plugin){
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(!(sender instanceof Player p)){
+            sender.sendMessage(Core.color(plugin.getConfig().getString("must-be-player")));
+            return true;
+        }
+        if (args.length == 0){
+            if(!(p.hasPermission("core.command.god"))) {
+                p.sendMessage(Core.color(plugin.getConfig().getString("no-permission")));
+                return true;
+            }
+            if (plugin.hasGodPlayers()){
+                if (plugin.getGodPlayers().contains(p)){
+                    plugin.removeGodPlayer(p);
+                    p.sendMessage(Core.color(plugin.getConfig().getString("god-false")));
+                }
+            } else {
+                plugin.addGodPlayer(p);
+                p.sendMessage(Core.color(plugin.getConfig().getString("god-true")));
+            }
+        } else if (args.length == 1){
+            if (!(p.hasPermission("core.command.god.others"))){
+                p.sendMessage(Core.color(plugin.getConfig().getString("no-permission")));
+                return true;
+            }
+            if (Bukkit.getPlayerExact(args[0]) == null){
+                sender.sendMessage(Core.color(plugin.getConfig().getString("player-not-found")));
+                return true;
+            }
+            Player t = Bukkit.getPlayer(args[0]);
+            if (plugin.hasGodPlayers()){
+                if (plugin.getGodPlayers().contains(t)){
+                    plugin.removeGodPlayer(t);
+                    t.sendMessage(Core.color(plugin.getConfig().getString("god-false-byothers") + sender.getName()));
+                    sender.sendMessage(Core.color(plugin.getConfig().getString("god-false-other") + t.getName()));
+                }
+            } else {
+                plugin.addGodPlayer(t);
+                t.sendMessage(Core.color(plugin.getConfig().getString("god-true-byothers") + sender.getName()));
+                sender.sendMessage(Core.color(plugin.getConfig().getString("god-true-other") + t.getName()));
+            }
+        }
+        return true;
+    }
+}
